a9ea1a2a86e7a227289590f745acb172aa0cbbd0
Prakhar Srivastav <prakhar1989@gmail.com>
2014-07-26 11:53:10 +0530

    Type definitions

--- lists.ml
+++ lists.ml
@@ -42,16 +42,40 @@ let render_table header rows =
     ((sep :: render_row header widths :: 
       sep :: List.map rows ~f:(fun x -> render_row x widths)) @ [sep])
 
+(* filter map removes all values that return a None *)
+let filenames = 
+  List.filter_map (Sys.ls_dir ".") 
+    ~f: (fun filename ->
+      match String.rsplit2 ~on:'.' filename with
+      | None -> None
+      | Some ("", _) -> None
+      | Some (_, ext) -> Some ext)
+    |> List.dedup
+
+let is_ocaml_source string = 
+  match String.rsplit2 string ~on:'.' with
+  | Some (_, ("ml"|"mli")) -> true
+  | _ -> false
+
+let (ml_files, other_files) =
+  List.partition_tf (Sys.ls_dir ".") ~f:is_ocaml_source
+
+(* tail call recursion *)
+let rec length_plus_n list n = 
+  match list with
+  | [] -> n
+  | _ :: tl -> length_plus_n tl (n + 1)
+
+let rec destutter = function
+  | [] | [_] as l -> l
+  | hd :: (hd' :: _ as tl) when hd = hd' -> destutter tl
+  | hd :: tl -> hd :: destutter tl
+
 let () =
   let header = ["Language";"Architect";"First Release"] in
   let rows = [ 
      ["Lisp"    ; "John McCarthy"    ; "1958"] ;
-     ["C"       ; "Dennis Ritchie"   ; "1969"] ;
-     ["Python"  ; "Guido Van Rossum" ; "1991"] ;
      ["Scala"   ; "Martin Odersky"   ; "2003"] ;
-     ["Clojure" ; "Rich Hickey"      ; "2007"] ;
-     ["ML"      ; "Robin Milner"     ; "1973"] ;
      ["OCaml"   ; "Xavier Leroy"     ; "1996"] ;
   ] in
   print_endline (render_table header rows)
-  
--- textastable.ml
+++ textastable.ml
@@ -1,40 +1,43 @@
 open Core.Std
 
-let max_length header rows = 
+(* type definitions *)
+type header = string list
+type rows = string list list
+
+(* implementation *)
+let max_length header rows : (int list) = 
   let lengths list = List.map ~f:String.length list in
   List.fold rows
     ~init: (lengths header)
     ~f: (fun acc row ->
       List.map2_exn ~f:Int.max acc (lengths row))
 
-let make_separator widths = 
+let make_separator (widths: int list) : (string) = 
   let pieces = List.map widths 
     ~f:(fun w -> String.make (w + 2) '-') (* padding of 2 chars *)
   in
   "+" ^ String.concat ~sep:"+" pieces ^ "+"
 
-let pad string width = 
-  " " ^ string ^ String.make (width - String.length string + 1) ' '
+let pad (s: string) (width: int) : (string) = 
+  " " ^ s ^ String.make (width - String.length s + 1) ' '
 
-let render_row row widths =
+let render_row (row: string list) (widths: int list) : (string) =
   let items = List.map2_exn row widths ~f:pad in 
   "|" ^ String.concat items ~sep:"|" ^ "|"
 
-let render_table header rows =
+let render_table header rows : (string) =
   let widths = max_length header rows in
   let sep = make_separator widths in
   String.concat ~sep:"\n" 
     ((sep :: render_row header widths :: 
       sep :: List.map rows ~f:(fun x -> render_row x widths)) @ [sep])
 
+(* main *)
 let () =
   let header = ["Language";"Architect";"First Release"] in
   let rows = [ 
      ["Lisp"    ; "John McCarthy"    ; "1958"] ;
-     ["C"       ; "Dennis Ritchie"   ; "1969"] ;
-     ["Python"  ; "Guido Van Rossum" ; "1991"] ;
      ["Scala"   ; "Martin Odersky"   ; "2003"] ;
-     ["Clojure" ; "Rich Hickey"      ; "2007"] ;
      ["ML"      ; "Robin Milner"     ; "1973"] ;
      ["OCaml"   ; "Xavier Leroy"     ; "1996"] ;
   ] in
